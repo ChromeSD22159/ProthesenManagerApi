9af8357bf4225087d3acbd53d16913220fb41f43
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-05-14 09:57:52 +0200

    optimize

--- src/main/kotlin/de/frederikkohler/routes/profileRoutes.kt
+++ src/main/kotlin/de/frederikkohler/routes/profileRoutes.kt
@@ -14,7 +14,7 @@ fun Routing.profileRoute(profileService: ProfileService) {
     put("/profile/{id}"){
         try {
             val user=call.receive<Profile>()
-            v   al result=profileService.updateProfile(user)
+            val result=profileService.updateProfile(user)
             if (result){
                 call.respond(HttpStatusCode.OK,"Update successful")
             }else{
