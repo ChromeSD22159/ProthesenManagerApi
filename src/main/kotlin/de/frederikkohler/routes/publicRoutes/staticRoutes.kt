12b08a4180abe78e7fba63d9b6894f012417808a
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-05-22 13:57:40 +0200

    staticRoutes update

--- src/main/kotlin/de/frederikkohler/routes/publicRoutes/staticRoutes.kt
+++ src/main/kotlin/de/frederikkohler/routes/publicRoutes/staticRoutes.kt
@@ -1,10 +1,18 @@
 package de.frederikkohler.routes.publicRoutes
 
+import io.ktor.server.auth.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import java.io.File
 
-fun Routing.staticRoutes(){
-    static("/uploads") {
-        files("uploads")
+fun Routing.staticRoutes(dev: Boolean) {
+    if (dev) {
+        staticFiles("/uploads", File("uploads"))
+    } else {
+        authenticate {
+            staticFiles("/uploads", File("uploads"))
+        }
     }
+
 }
+
